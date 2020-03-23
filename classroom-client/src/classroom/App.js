34080ffde810fb1922375cd0cea1e01678203b93
Neil Manansala <learlessfeader@gmail.com>
2020-03-23 14:37:57 -0700

    Set up the frontend stack

---
+++ classroom-client/src/classroom/App.js
@@ -0,0 +1,8 @@
+import React from "react"
+import styles from "./App.scss"
+
+export default class App extends React.Component {
+    render() {
+        return <div className={styles.test}>Hello, React!</div>
+    }
+}
--- classroom-client/src/main.js
+++ classroom-client/src/main.js
@@ -1,14 +1,24 @@
+const path = require("path");
+const url = require("url");
 const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
     let browserWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1024,
+        height: 768,
+        minWidth: 1024,
+        minHeight: 768,
+        show: false,
         webPreferences: {
             nodeIntegration: true
         }
     });
-    browserWindow.loadFile("index.html");
+    browserWindow.loadURL(url.format({
+        protocol: "file",
+        slashes: false,
+        pathname: path.resolve(__dirname, "..", "build", "index.html")
+    }));
+    browserWindow.once("ready-to-show", () => browserWindow.show());
 }
 
 app.whenReady().then(createWindow);
