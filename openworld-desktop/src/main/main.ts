d9bcc52de60742bf53054cfdd1a357355a3d6fa1
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-08-05 22:05:53 -0300

    eslint

--- openworld-desktop/src/main/main.ts
+++ openworld-desktop/src/main/main.ts
@@ -3,37 +3,39 @@ import url from "url";
 import { app, BrowserWindow } from "electron";
 
 function createWindow() {
-    let browserWindow = new BrowserWindow({
-        width: 1024,
-        height: 768,
-        minWidth: 1024,
-        minHeight: 768,
-        show: false,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
-    browserWindow.loadURL(url.format({
-        protocol: "file",
-        slashes: false,
-        pathname: path.resolve(__dirname, "renderer", "index.html")
-    }));
-    browserWindow.once("ready-to-show", () => {
-        browserWindow.show();
-        browserWindow.webContents.openDevTools();
-    });
+  const browserWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    minWidth: 1024,
+    minHeight: 768,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  browserWindow.loadURL(
+    url.format({
+      protocol: "file",
+      slashes: false,
+      pathname: path.resolve(__dirname, "renderer", "index.html"),
+    })
+  );
+  browserWindow.once("ready-to-show", () => {
+    browserWindow.show();
+    browserWindow.webContents.openDevTools();
+  });
 }
 
 app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
-    if(process.platform !== "darwin") {
-        app.quit();
-    }
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 app.on("activate", () => {
-    if(BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
