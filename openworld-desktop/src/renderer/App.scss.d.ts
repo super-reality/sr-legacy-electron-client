1b867472de07195979b7f23c4b4d28ed6f3a4005
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-08-05 21:51:43 -0300

    fix webpack

---
+++ openworld-desktop/src/renderer/App.scss.d.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+// GENERATED FILE; DO NOT EDIT
+declare namespace AppScssNamespace {
+  export interface IAppScss {
+    active: string;
+    nav: string;
+  }
+}
+
+declare const AppScssModule: AppScssNamespace.IAppScss & {
+  /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
+  locals: AppScssNamespace.IAppScss;
+};
+
+export = AppScssModule;
--- openworld-desktop/webpack/webpack.prod.js
+++ openworld-desktop/webpack/webpack.prod.js
@@ -1,8 +1,8 @@
 process.env.NODE_ENV = "production";
 
 const merge = require("webpack-merge");
-const common = require("./webpack/webpack.common.js");
-const post = require("./webpack/webpack.post.js");
+const common = require("./webpack.common.js");
+const post = require("./webpack.post.js");
 
 console.log("MODE: production");
 
