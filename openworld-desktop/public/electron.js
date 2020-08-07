29fae5a3243a40b42e3854b196accdd157320e5f
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-08-07 13:35:45 -0300

    builder

--- openworld-desktop/public/electron.js
+++ openworld-desktop/public/electron.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const { app, globalShortcut, BrowserWindow } = require("electron");
 
 const path = require("path");
@@ -18,7 +19,7 @@ function createWindow() {
   mainWindow.loadURL(
     process.env.ELECTRON_START_URL ||
       url.format({
-        pathname: path.join(__dirname, "/../public/index.html"),
+        pathname: path.join(__dirname, "..", "build", "index.html"),
         protocol: "file:",
         slashes: true,
       })
