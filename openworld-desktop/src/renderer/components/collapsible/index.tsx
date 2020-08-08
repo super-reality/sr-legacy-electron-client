a05a9987f3c20185c52ff8ab43b95ae082b81e62
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-08-08 10:56:43 -0300

    react spring animation

--- openworld-desktop/src/renderer/components/collapsible/index.tsx
+++ openworld-desktop/src/renderer/components/collapsible/index.tsx
@@ -1,19 +1,34 @@
-import React, {PropsWithChildren, useState, useCallback} from "react";
+import React, {PropsWithChildren, useState, useCallback, useEffect} from "react";
 import "./index.scss";
 import { ReactComponent as DropArrow } from "../../../assets/svg/drop.svg";
+import { animated, useSpring } from "react-spring";
+import { useMeasure } from "react-use";
 
 interface CollapsibleProps {
   title: string;
+  expanded?: boolean;
 }
 
 export default function Collapsible(
   props: PropsWithChildren<CollapsibleProps>
 ): JSX.Element {
-  const [expanded, setExpanded] = useState(true);
+  const [expanded, setExpanded] = useState(props.expanded ? true : false);
 
   const toggleExpanded = useCallback(() => {
     setExpanded(!expanded);
   }, [expanded]);
+
+  
+  const [contentHeight, setContentHeight] = useState(0);
+  const [ref, { height }] = useMeasure<HTMLDivElement>();
+
+  const expand = useSpring({
+    height: expanded ? `${contentHeight}px` : "0px"
+  });
+
+  useEffect(() => {
+    setContentHeight(height);
+  }, [height]);
 
   return (
     <>
@@ -25,9 +40,11 @@ export default function Collapsible(
           {props.title}
         </div>
       </div>
-      <div className={`collapsible-content ${expanded ? "expanded" : ""}`}>
-        {props.children}
-      </div>
+      <animated.div style={expand} className={`collapsible-content`}>
+        <div ref={ref}>
+          {props.children}
+        </div>
+      </animated.div>
     </>
   );
 }
