1498de16af3f55029582cbf809a54e75832a37e7
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-08-10 20:02:04 -0300

    top select state

--- openworld-desktop/src/renderer/redux/slices/renderSlice.ts
+++ openworld-desktop/src/renderer/redux/slices/renderSlice.ts
@@ -1,8 +1,9 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 const initialState = {
   yScroll: 0,
   yScrollDelta: 0,
+  topSelectStates: {} as Record<string, string>,
 };
 
 type RenderState = typeof initialState;
@@ -11,18 +12,22 @@ const renderSlice = createSlice({
   name: "render",
   initialState,
   reducers: {
-    setYScroll: (
-      state: RenderState,
-      action: PayloadAction<number>
-    ): void => {
+    setYScroll: (state: RenderState, action: PayloadAction<number>): void => {
       state.yScrollDelta = state.yScroll - action.payload;
       state.yScroll = action.payload;
-    }
+    },
+    setTopSelect: (
+      state: RenderState,
+      action: PayloadAction<{selected: string; path: string}>
+    ): void => {
+      state.topSelectStates = {
+        ...state.topSelectStates,
+        [action.payload.path]: action.payload.selected,
+      };
+    },
   },
 });
 
-export const {
-  setYScroll
-} = renderSlice.actions;
+export const {setYScroll, setTopSelect} = renderSlice.actions;
 
 export default renderSlice;
