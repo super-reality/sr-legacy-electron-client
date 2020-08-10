769c85166a04f83976efd8913ee479699a7720e5
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-08-10 13:03:09 -0300

    top nav search and select conditional

--- openworld-desktop/src/renderer/components/top-search/index.tsx
+++ openworld-desktop/src/renderer/components/top-search/index.tsx
@@ -3,11 +3,44 @@ import "./index.scss";
 import {ReactComponent as SearchIcon} from "../../../assets/svg/search.svg";
 import {ReactComponent as BackIcon} from "../../../assets/svg/back.svg";
 import {ReactComponent as AlertIcon} from "../../../assets/svg/alert.svg";
+import Select from "../select";
 import {useSelector} from "react-redux";
 import {AppState} from "../../redux/stores/renderer";
-import { useSpring, animated } from "react-spring";
+import {useSpring, animated} from "react-spring";
+import {useLocation} from "react-router-dom";
+import Flex from "../flex";
+
+const selectOptionsByTab: Record<string, string[]> = {
+  "/find": [
+    "All",
+    "Subjects",
+    "Organization",
+    "Collection",
+    "Teachers",
+    "Student",
+    "Wanted",
+    "Teacher Bot",
+  ],
+  "/learn": [
+    "All My Stuff",
+    "My Lessons",
+    "My Subjects",
+    "My Organizations",
+    "My Collections",
+    "My Teachers",
+    "My Classmates",
+    "My Teacher Bot",
+  ],
+};
 
 export default function TopSearch(): JSX.Element {
+  const location = useLocation();
+  const currentOptions = selectOptionsByTab[location.pathname];
+  const showSelect = currentOptions ? true : false;
+  const [currentSelected, setCurrentSelected] = useState(
+    showSelect ? currentOptions[0] : ""
+  );
+
   const offset = 45;
   const {yScroll, yScrollDelta} = useSelector(
     (state: AppState) => state.render
@@ -24,24 +57,41 @@ export default function TopSearch(): JSX.Element {
 
   const spring = useSpring({top: `${yPos}px`});
 
+  useEffect(() => {
+    setCurrentSelected(showSelect ? currentOptions[0] : "");
+  }, [location]);
+
   return (
-    <animated.div style={spring} className="top-controls">
+    <animated.div style={spring} className={`top-controls ${!showSelect ? "no-select" : ""}`}>
       <BackIcon
-        style={{margin: "auto 0 auto 8px"}}
+        style={{margin: "auto"}}
         width="42px"
         fill="var(--color-section)"
       />
-      <div className="top-input-container">
-        <input className="top-input" />
-        <SearchIcon
-          style={{margin: "auto 8px"}}
-          width="20px"
-          height="20px"
-          fill="var(--color-text)"
+      <Flex>
+        <div className="top-input-container">
+          <input className="top-input" />
+            <div className={"top-inpu-icon"}>
+            <SearchIcon
+              width="20px"
+              height="20px"
+              fill="var(--color-text)"
+            />
+          </div>
+        </div>
+      </Flex>
+      {currentOptions ? (
+        <Select
+          style={{width: "auto"}}
+          current={currentSelected}
+          callback={setCurrentSelected}
+          options={currentOptions}
         />
-      </div>
+      ) : (
+        <></>
+      )}
       <AlertIcon
-        style={{margin: "auto 8px auto 0"}}
+        style={{margin: "auto"}}
         width="42px"
         fill="var(--color-section)"
       />
