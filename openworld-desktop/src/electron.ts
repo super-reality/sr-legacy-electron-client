a9280b98f63115a91d2fce4e1f71ac535b0ee1a4
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-08-10 10:15:49 -0300

    electron module require

---
+++ openworld-desktop/src/electron.ts
@@ -0,0 +1,12 @@
+const electron = window.require("electron");
+
+export default electron;
+
+/*
+https://github.com/electron/electron/issues/6451#issuecomment-332384546
+
+import electron from "./electron";
+const { remote } = electron;
+remote.app.getVersion()
+
+*/
