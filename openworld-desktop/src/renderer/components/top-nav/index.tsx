22691acd7ee51199b9ced8376f35132b1efaed51
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-08-11 12:13:45 -0300

    scroll top nav

--- openworld-desktop/src/renderer/components/top-nav/index.tsx
+++ openworld-desktop/src/renderer/components/top-nav/index.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import "./index.scss";
 import { NavLink, useLocation } from "react-router-dom";
 import { useSpring, animated } from "react-spring";
+import { useMeasure } from "react-use";
 
 interface TopNavItemProps {
   title: string;
@@ -28,30 +29,47 @@ function TopNavItem(props: TopNavItemProps): JSX.Element {
   );
 }
 
+const navItemSize = 64;
+
 export default function TopNav(): JSX.Element {
   const location = useLocation();
+  const [xScroll, setXScroll] = useState(0);
+  const [ref, { width }] = useMeasure<HTMLDivElement>();
+
+  const diff = (topNavButtons.length * navItemSize) - width;
 
   let from = "0px";
   topNavButtons.forEach((b, index) => {
     const isActive = location.pathname === b[0];
     if (isActive) {
-      from = `${index * 64}px`;
+      from = `${index * navItemSize}px`;
     }
   });
+
+  if (xScroll !== 0 && diff <= 0) {
+    setXScroll(0);
+  }
+
+  const handleScroll = useCallback((event: React.WheelEvent<HTMLDivElement>) => {
+    let newScroll = Math.min(0, xScroll + (event.deltaY / 5));
+    newScroll = Math.max(diff * -1, newScroll);
+    setXScroll(newScroll);
+    // eslint-disable-next-line
+  }, [xScroll]);
 
   const props = useSpring({ marginLeft: from });
 
   return (
-    <div className={"topnav-container"}>
-      <div>
-        <div className={"topnav-navs-container"}>
+    <div ref={ref} className={"topnav-container"}>
+      <div onWheel={handleScroll}>
+        <div style={{ left: `${xScroll}px` }} className={"topnav-navs-container"}>
           {topNavButtons.map((b) => (
             <TopNavItem route={b[0]} title={b[1]} />
           ))}
         </div>
         <div
           className={"topnav-indicator-container"}
-          style={{ width: `${topNavButtons.length * 64}px` }}
+          style={{ left: `${xScroll}px`, width: `${topNavButtons.length * navItemSize}px` }}
         >
           <animated.div className={"topnav-indicator"} style={props} />
         </div>
