244e82e8896661cb07b9d1ac713efea4328e86b5
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-08-11 17:21:08 -0300

    checked icons

--- openworld-desktop/src/renderer/components/check-button/index.tsx
+++ openworld-desktop/src/renderer/components/check-button/index.tsx
@@ -1,6 +1,7 @@
 import React, {CSSProperties} from "react";
 import "../buttons.scss";
 
+import {ReactComponent as UncheckIcon} from "../../../assets/svg/unchecked.svg";
 import {ReactComponent as CheckIcon} from "../../../assets/svg/checked.svg";
 
 interface Shareprops {
@@ -13,13 +14,24 @@ export default function CheckButton(props: Shareprops): JSX.Element {
   return (
     <div
       className="icon-button"
-      style={{...props.style, display: "flex", width: "20px"}}
+      style={{...props.style, display: "flex", width: "20px", height: "20px"}}
       onClick={props.callback}
     >
-      <CheckIcon
-        fill={props.checked ? "var(--color-checked)" : "var(--color-text)"}
-        style={{margin: "auto"}}
-      />
+      {props.checked ? (
+        <CheckIcon
+          fill={props.checked ? "var(--color-checked)" : "var(--color-text)"}
+          width="20px"
+          height="20px"
+          style={{margin: "auto"}}
+        />
+      ) : (
+        <UncheckIcon
+          fill={props.checked ? "var(--color-checked)" : "var(--color-text)"}
+          width="20px"
+          height="20px"
+          style={{margin: "auto"}}
+        />
+      )}
     </div>
   );
 }
