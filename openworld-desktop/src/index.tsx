0bd06be8f6b2009fa2cedc96c2e768c927bdd4d6
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-08-13 12:07:57 -0300

    fix hot reload for components

--- openworld-desktop/src/index.tsx
+++ openworld-desktop/src/index.tsx
@@ -17,6 +17,22 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+if (module.hot && process.env.NODE_ENV === "development") {
+  module.hot.accept();
+  // eslint-disable-next-line global-require
+  const NextApp = require("./renderer/App").default;
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <Router>
+          <NextApp />
+        </Router>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
