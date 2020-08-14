8c2cb88c41149916ed471e037ee9fcb605d1f06c
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-08-14 13:46:43 -0300

    knight rider loader

--- openworld-desktop/src/renderer/components/loading/index.tsx
+++ openworld-desktop/src/renderer/components/loading/index.tsx
@@ -4,7 +4,13 @@ import "./index.scss";
 export default function Loading() {
   return (
     <div className="loader-container">
-      <div className="loader" />
+      <div className="spinner">
+        <div className="rect1" />
+        <div className="rect2" />
+        <div className="rect3" />
+        <div className="rect4" />
+        <div className="rect5" />
+      </div>
     </div>
   );
 }
