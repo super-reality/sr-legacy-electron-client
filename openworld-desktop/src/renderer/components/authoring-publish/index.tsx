2f826706c7baaf28657d67f81457c5f159d9ee29
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-08-19 12:43:48 -0300

    autosuggest

--- openworld-desktop/src/renderer/components/authoring-publish/index.tsx
+++ openworld-desktop/src/renderer/components/authoring-publish/index.tsx
@@ -1,38 +1,100 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useMemo } from "react";
 import "../create-lesson/index.scss";
 import "../containers.scss";
 import Flex from "../flex";
-import Select from "../select";
 import ButtonSimple from "../button-simple";
+import AutosuggestInput from "../autosuggest-input";
 
-const visibilityOptions = ["public", "friends", "private"];
+interface Lang {
+  name: string;
+  year: number;
+}
 
-const entryOptions = ["bid", "free", "fixed", "range"];
+const languages: Lang[] = [
+  {
+    name: "C",
+    year: 1972,
+  },
+  {
+    name: "C#",
+    year: 2000,
+  },
+  {
+    name: "C++",
+    year: 1983,
+  },
+  {
+    name: "Clojure",
+    year: 2007,
+  },
+  {
+    name: "Elm",
+    year: 2012,
+  },
+  {
+    name: "Go",
+    year: 2009,
+  },
+  {
+    name: "Haskell",
+    year: 1990,
+  },
+  {
+    name: "Java",
+    year: 1995,
+  },
+  {
+    name: "JavaScript",
+    year: 1995,
+  },
+  {
+    name: "Perl",
+    year: 1987,
+  },
+  {
+    name: "PHP",
+    year: 1995,
+  },
+  {
+    name: "Python",
+    year: 1991,
+  },
+  {
+    name: "Ruby",
+    year: 1995,
+  },
+  {
+    name: "Scala",
+    year: 2003,
+  },
+];
+
+const getVal = (suggestion: Lang) => suggestion.name;
+const renderVal = (suggestion: Lang) => <div>{suggestion.name}</div>;
+
+// Teach Autosuggest how to calculate suggestions for any given input value.
+const getVals = (str: string) => {
+  const inputValue = str.trim().toLowerCase();
+
+  return inputValue.length === 0
+    ? []
+    : languages.filter(
+        (lang) => lang.name.toLowerCase().indexOf(inputValue) > -1
+      );
+};
 
 export default function PublishAuthoring(): JSX.Element {
-  const [title, setTitle] = useState("");
-
-  const handleChange = useCallback(
-    (
-      e:
-        | React.ChangeEvent<HTMLInputElement>
-        | React.KeyboardEvent<HTMLInputElement>
-    ): void => {
-      setTitle(e.currentTarget.value);
-    },
-    []
-  );
-
   return (
     <>
       <Flex>
         <div className="container-with-desc">
           <div>Parent Subject</div>
-          <input
-            placeholder="Title"
-            value={title}
-            onChange={handleChange}
-            onKeyDown={handleChange}
+          <AutosuggestInput<Lang>
+            getValue={getVal}
+            renderSuggestion={renderVal}
+            filter={getVals}
+            id="parent-subject"
+            submitCallback={(l) => console.log(l)}
           />
         </div>
       </Flex>
