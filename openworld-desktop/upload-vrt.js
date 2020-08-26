26fb84a68488e8a7e8cf5b3cb8f5e7b96c025317
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-08-26 11:31:32 -0300

    upload vrt results to imgur

---
+++ openworld-desktop/upload-vrt.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const fs = require("fs");
+const imgur = require("imgur");
+
+imgur.setClientId("037c98456c9f3dd");
+const snapshotsDir = "./src/__tests__/__image_snapshots__/__diff_output__/";
+
+function tryUpload(file) {
+  imgur
+    .uploadFile(snapshotsDir + file)
+    .then((json) => {
+      console.log(`Uploaded ${file} to: ${json.data.link}`);
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
+}
+
+fs.readdir(snapshotsDir, (err, files) => {
+  files.forEach(tryUpload);
+});
