6be5ee410f508f27a8553fb6b90f335249e16d50
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-08-27 15:02:18 -0300

    Update utils.ts

--- openworld-desktop/src/utils/__tests__/utils.ts
+++ openworld-desktop/src/utils/__tests__/utils.ts
@@ -1,30 +1,25 @@
 /* eslint-env jest */
 import path from "path";
-import Splash from "../../assets/images/splash.png";
 import getFileExt from "../getFileExt";
 import getFileSha1 from "../getFileSha1";
 import uploadFileToS3 from "../uploadFileToS3";
 
+const file = path.join(__dirname, "..", "..", "assets", "images", "splash.png");
+
 test("Can get a file extension", () => {
-  expect(getFileExt(Splash)).toBe(".png");
+  expect(getFileExt(file)).toBe(".png");
   expect(getFileExt("test.txt")).toBe(".txt");
   expect(getFileExt("filenamewith/no/extension")).toBe("");
 });
 
 test("Can get a file sha1 hash", () => {
-  expect(getFileSha1(Splash)).toBe("da39a3ee5e6b4b0d3255bfef95601890afd80709");
+  expect(getFileSha1(file)).toBe("da39a3ee5e6b4b0d3255bfef95601890afd80709");
 });
+
 /*
 test("Can upload to aws", async (done) => {
   let stat = false;
-  const file = path.join(
-    __dirname,
-    "..",
-    "..",
-    "assets",
-    "images",
-    "splash.png"
-  );
+  
   await uploadFileToS3(file)
     .then(() => {
       stat = true;
