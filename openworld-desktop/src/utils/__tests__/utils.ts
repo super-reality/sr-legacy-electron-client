e581cc0c61cbc4ffe1f0b725833a21cb261f8f93
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-08-27 18:41:35 -0300

    test timeout

--- openworld-desktop/src/utils/__tests__/utils.ts
+++ openworld-desktop/src/utils/__tests__/utils.ts
@@ -4,6 +4,8 @@ import getFileExt from "../getFileExt";
 import getFileSha1 from "../getFileSha1";
 import uploadFileToS3 from "../uploadFileToS3";
 
+jest.setTimeout(30000);
+
 const file = path.join(__dirname, "..", "..", "assets", "images", "splash.png");
 
 test("Can get a file extension", () => {
@@ -16,10 +18,8 @@ test("Can get a file sha1 hash", () => {
   expect(getFileSha1(file)).toBe("7b9d27774fa36de33529ff6b2487cef0bee6d75a");
 });
 
-/*
 test("Can upload to aws", async (done) => {
   let stat = false;
-  
   await uploadFileToS3(file)
     .then(() => {
       stat = true;
@@ -29,4 +29,3 @@ test("Can upload to aws", async (done) => {
   expect(stat).toBe(true);
   done();
 });
-*/
