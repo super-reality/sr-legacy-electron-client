d4444551b881ed6aff4e7e9f66dca0f074560471
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-09-04 14:40:12 -0300

    Api Join types

--- openworld-desktop/src/renderer/hooks/useEditButton.tsx
+++ openworld-desktop/src/renderer/hooks/useEditButton.tsx
@@ -1,40 +1,16 @@
 import React, { useCallback } from "react";
 import Axios from "axios";
 import { ReactComponent as EditButton } from "../../assets/svg/edit.svg";
-import CollectionGet, { ICollectionGet } from "../api/types/collection/get";
-import useDataGet from "./useDataGet";
-import { ApiSucess, ApiError } from "../api/types";
+import { ApiError, ApiJoin } from "../api/types";
 import handleGenericGet from "../api/handleGenericGet";
 import { API_URL } from "../constants";
-import SubjectGet, { ISubjectGet } from "../api/types/subject/get";
-import LessonGet, { ILessonGet } from "../api/types/lesson/get";
+import { ICollectionGet } from "../api/types/collection/get";
 
 function triggerEditCollection(data: ICollectionGet) {
   console.log("triggerEditCollection", data);
 }
 
-interface CollectionArg {
-  type: "collection";
-  id?: string;
-  data?: ICollectionGet;
-  api?: CollectionGet;
-}
-
-interface SubjectArg {
-  type: "subject";
-  id?: string;
-  data?: ISubjectGet;
-  api?: SubjectGet;
-}
-
-interface LessonArg {
-  type: "lesson";
-  id?: string;
-  data?: ILessonGet;
-  api?: LessonGet;
-}
-
-type EditArg = CollectionArg | SubjectArg | LessonArg;
+type EditArg = ApiJoin & { id: string };
 
 function fetchData<A>(arg: EditArg): Promise<A> {
   return new Promise((resolve, reject) => {
@@ -48,18 +24,13 @@ function fetchData<A>(arg: EditArg): Promise<A> {
 }
 
 export default function useEditButton(arg: EditArg): () => JSX.Element {
-  const [data] = useDataGet<ApiSucess, typeof arg["data"]>(
-    arg.type,
-    arg.id || ""
-  );
-
   const callback = useCallback(() => {
     if (arg.type == "collection") {
       fetchData<NonNullable<typeof arg["api"]>>(arg).then((getData) => {
         triggerEditCollection(getData.collection);
       });
     }
-  }, [data]);
+  }, []);
 
   const Component = () => (
     <div
