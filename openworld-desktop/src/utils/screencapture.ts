d5dc044684a67ebfe5d8109a8d1828880fdb26ca
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-09-11 20:08:04 -0300

    proper nircmdc locations

--- openworld-desktop/src/utils/screencapture.ts
+++ openworld-desktop/src/utils/screencapture.ts
@@ -8,8 +8,8 @@ const { remote } = require("electron");
 const proc: any = process;
 // freeware nircmd http://www.nirsoft.net/utils/nircmd.html
 const nircmdc = remote.app.isPackaged
-  ? path.join(proc.resourcesPath, "app.asar", "build", "nircmdc.exe")
-  : path.join(remote.app.getAppPath(), "public", "nircmdc.exe");
+  ? path.join(proc.resourcesPath, "extra", "nircmdc.exe")
+  : path.join(remote.app.getAppPath(), "public", "extra", "nircmdc.exe");
 
 function captureCommand(filePath: string) {
   switch (os.platform()) {
@@ -26,7 +26,7 @@ function captureCommand(filePath: string) {
 
 function capture(filePath: string, callback: any) {
   exec(captureCommand(filePath), (err) => {
-    console.error(err);
+    // console.error(err);
     // nircmd always exits with err even though it works
     if (err && os.platform() !== "win32") callback(err);
 
