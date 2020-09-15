a82b62aa973db2da12a0ef4ae39a9a68722c8d8c
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-09-15 18:40:46 -0300

    Update useCVMatch.tsx

--- openworld-desktop/src/renderer/hooks/useCVMatch.tsx
+++ openworld-desktop/src/renderer/hooks/useCVMatch.tsx
@@ -17,13 +17,13 @@ function getTemplateMat(id: string, xScale: number, yScale: number): cv.Mat {
   const h = img.height;
   canvas.width = w;
   canvas.height = h;
-  console.log(w, h);
+  // console.log(w, h);
   const ctx = canvas.getContext("2d");
   if (ctx) {
     ctx.drawImage(img, 0, 0);
     const buff = ctx.getImageData(0, 0, w, h).data;
-    const mat = new cv.Mat(Buffer.from(buff), w, h, cv.CV_8UC4);
-    console.log(w / xScale, h / yScale);
+    const mat = new cv.Mat(Buffer.from(buff), h, w, cv.CV_8UC4);
+    // console.log(w / xScale, h / yScale);
     return mat.resize(Math.round(h / yScale), Math.round(w / xScale));
   }
   return new cv.Mat();
@@ -66,8 +66,8 @@ function matToCanvas(mat: cv.Mat, id: string): void {
   // set canvas dimensions
   const canvas = document.getElementById(id) as HTMLCanvasElement;
   if (canvas) {
-    canvas.height = mat.rows;
     canvas.width = mat.cols;
+    canvas.height = mat.rows;
   }
 
   // set image data
@@ -131,7 +131,7 @@ export default function useCVMatch(
       )
         return;
 
-      // set canvas dimensions
+      // get canvas for the ourput
       const canvas = document.getElementById(
         "canvasOutput"
       ) as HTMLCanvasElement;
@@ -154,174 +154,69 @@ export default function useCVMatch(
         const xScale = videoElement.current.videoWidth / width;
         const yScale = videoElement.current.videoHeight / height;
 
-        console.log(`${width} x ${height}`);
         // Draw video onto a new canvas and get the buffer data to a Mat
 
         ctx.drawImage(videoElement.current, 0, 0, width, height);
 
         const buffer = Buffer.from(ctx.getImageData(0, 0, width, height).data);
         let srcMat = new cv.Mat(buffer, height, width, cv.CV_8UC4);
-        srcMat = srcMat.cvtColor(cv.COLOR_RGBA2BGRA);
-        // srcMat = srcMat.cvtColor(cv.COLOR_RGBA2GRAY);
+        // srcMat = srcMat.cvtColor(cv.COLOR_RGBA2BGRA);
+        srcMat = srcMat.cvtColor(cv.COLOR_RGBA2GRAY);
 
         if (srcMat) {
           // Template
-          const templateMat = getTemplateMat("templateImage", xScale, yScale); // .cvtColor(cv.COLOR_RGBA2GRAY);
+          const templateMat = getTemplateMat(
+            "templateImage",
+            xScale,
+            yScale
+          ).cvtColor(cv.COLOR_RGBA2GRAY);
 
-          console.log(templateMat);
+          // console.log(templateMat);
           // Do match
           const result = srcMat.matchTemplate(
             templateMat,
             cv.TM_CCORR_NORMED,
             new cv.Mat()
           );
-          // result.normalize(cv.NORM_MINMAX);
-          result.normalize(0, 1, cv.NORM_MINMAX, -1, new cv.Mat());
-          const minMax = result.minMaxLoc();
-          console.log(minMax.maxLoc);
 
+          // result.normalize(0, 1, cv.NORM_MINMAX, -1, new cv.Mat());
+          const minMax = result.minMaxLoc();
+
+          const bestPoint = minMax.maxLoc;
+          const bestDist = minMax.maxVal;
           srcMat.drawRectangle(
             minMax.maxLoc,
             new cv.Point2(
-              minMax.maxLoc.x + templateMat.cols,
-              minMax.maxLoc.y + templateMat.rows
+              bestPoint.x + templateMat.cols,
+              bestPoint.y + templateMat.rows
             ),
             new cv.Vec3(0, 255, 0),
             5
           );
-          /*
-          createFindBox({
-            x: Math.round(xScale * minMax.maxLoc.x),
-            y: Math.round(yScale * minMax.maxLoc.y),
-            width: Math.round(templateMat.cols * xScale),
-            height: Math.round(templateMat.rows * yScale),
-          });
-          */
-          matToCanvas(srcMat, "canvasOutput");
+
+          if (bestDist > opt.threshold) {
+            const ret: CVResult = {
+              dist: bestDist,
+              sizeFactor: 0,
+              x: Math.round(xScale * bestPoint.x),
+              y: Math.round(yScale * bestPoint.y),
+              width: Math.round(templateMat.cols * xScale),
+              height: Math.round(templateMat.rows * yScale),
+            };
+            callback(ret);
+          } else {
+            console.log(`not found: ${bestDist}`);
+          }
+
+          // matToCanvas(srcMat, "canvasOutput");
+        } else if (!capturing && !force) {
+          setTimeout(() => doMatch(true), 10);
+          // matToCanvas(srcMat, "canvasOutput");
         }
       } else {
-        console.log(canvas, ctx, videoElement.current);
+        console.error(canvas, ctx, videoElement.current);
       }
-
-      /*
-      const canvas = document.createElement("canvas") as HTMLCanvasElement;
-      const ctx = canvas.getContext("2d");
-      if (ctx && videoElement.current) {
-        ctx.drawImage(
-          videoElement.current,
-          0,
-          0,
-          opt.maxCanvasSize,
-          opt.maxCanvasSize
-        );
-      }
-      const srcMat = canvasToMat(canvas, opt.maxCanvasSize, opt.maxCanvasSize);
-      console.log(srcMat);
-      if (srcMat) {
-        srcMat.resize(opt.maxCanvasSize, opt.maxCanvasSize);
-
-        srcMat.cvtColor(cv.COLOR_RGBA2GRAY);
-        matToCanvas(srcMat, "canvasOutput");
-      }
-      */
-      /*
-      if (canvasEl.current && srcMat && templateEl.current) {
-        try {
-          const dstC1 = new cv.Mat(
-            opt.maxCanvasSize,
-            opt.maxCanvasSize,
-            cv.CV_8UC1
-          );
-          if (force || frames !== 0) {
-            // Original to grayscale
-            const vc = new cv.VideoCapture(0);
-            const frame = vc.read();
-            frame.cvtColor(cv.COLOR_RGBA2GRAY);
-
-            // Dest and mask
-            const dst = new cv.Mat(
-              opt.maxCanvasSize,
-              opt.maxCanvasSize,
-              cv.CV_8UC4
-            );
-            const mask = new cv.Mat();
-
-
-            // Template
-            const buffer = Buffer.from(getImageData("templateImage") || []);
-            const templateMat = cv.imdecode(buffer);
-            const tw = templateMat.cols / xScale;
-            const th = templateMat.rows / yScale;
-
-            templateMat.resize(tw, th);
-
-            console.log(
-              `Process ${dst.rows}x${dst.cols} (${dst.rows * dst.cols})`
-            );
-            const timerId = new Date().getTime();
-            console.time(`CV Loop ${timerId}`);
-
-            // Do match
-            const result = srcMat.matchTemplate(
-              templateMat,
-              cv.TM_CCORR_NORMED,
-              mask
-            );
-            // result.normalize(cv.NORM_MINMAX);
-            const minMax = result.minMaxLoc();
-
-            console.log(minMax);
-            console.timeEnd(`CV Loop ${timerId}`);
-            
-            // Re-scale to draw
-            const point = new cv.Point(
-              bestPoint.x + templateMat.cols,
-              bestPoint.y + templateMat.rows
-            );
-
-            // Output
-            const redScalar = new cv.Scalar(255, 0, 0, 255);
-            cv.rectangle(src, bestPoint, point, redScalar, 2, cv.LINE_8, 0);
-            cv.rectangle(
-              src,
-              { x: templateMat.cols / 2, y: templateMat.rows / 2 },
-              { x: dst.cols + templateMat.cols / 2, y: dst.rows + templateMat.rows / 2 },
-              redScalar,
-              2,
-              cv.LINE_8,
-              0
-            );
-
-            console.log("Best match rate: ", bestDist);
-            const size =
-              Math.sqrt(templateMat.cols * templateMat.rows) / opt.thresholdFactor;
-            console.log("Threshold: ", opt.threshold - size);
-
-            if (bestDist > opt.threshold - size) {
-              const result: CVResult = {
-                dist: bestDist,
-                sizeFactor: size,
-                x: Math.round(xScale * bestPoint.x),
-                y: Math.round(yScale * bestPoint.y),
-                width: Math.round(templateMat.cols * xScale),
-                height: Math.round(templateMat.rows * yScale),
-              };
-              callback(result);
-            }
-            matToCanvas(srcMat, "canvasOutput");
-          } else {
-            // First frame will always be empty
-            matToCanvas(dstC1, "canvasOutput");
-            if (!capturing && !force) {
-              setTimeout(() => doMatch(true), 10);
-            }
-          }
-        } catch (e) {
-          console.error(e);
-        }
-        setFrames(frames + 1);
-      }
-      */
+      setFrames(frames + 1);
     },
     [callback, capturing, frames, videoElement, canvasEl, templateEl]
   );
@@ -366,7 +261,7 @@ export default function useCVMatch(
     () => () => (
       <div
         style={{
-          display: "flex",
+          display: "none",
           flexDirection: "column",
           alignItems: "center",
         }}
