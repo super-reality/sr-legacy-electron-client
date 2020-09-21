5011abcbcdd76247cb8d24e3fdc8c3dee657d700
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-09-21 12:09:32 -0300

    profile sliders

--- openworld-desktop/src/renderer/redux/slices/settingsSlice.ts
+++ openworld-desktop/src/renderer/redux/slices/settingsSlice.ts
@@ -2,7 +2,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const initialState = {
-  cvThreshold: 0.99,
+  cvThreshold: 990,
   cvCanvas: 1024,
   cvDelay: 100,
 };
@@ -23,7 +23,7 @@ const settingsSlice = createSlice({
       state: SettingsState,
       action: PayloadAction<Partial<SettingsState>>
     ): void => {
-      state = { ...state, ...action };
+      state = Object.assign(state, action.payload);
     },
   },
 });
