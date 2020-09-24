0413d7c08ba99984b6f6a34360816aa5cd329f26
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-09-24 13:24:19 -0300

    Update constants.ts

--- openworld-desktop/src/renderer/constants.ts
+++ openworld-desktop/src/renderer/constants.ts
@@ -1,3 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
-export const API_URL = "http://54.183.151.100:8000/api/v1/";
+export const API_URL = "http://54.183.151.100:3000/api/v1/";
 export const timeout = 10000;
