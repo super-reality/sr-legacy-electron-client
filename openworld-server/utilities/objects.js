8cffe29e71fbd4aa5545fb3fdb2deeccb3260c2e
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-09-25 10:13:27 -0300

    init

--- openworld-server/utilities/objects.js
+++
@@ -1,7 +0,0 @@
-const filterKeys = (object, keys) => {
-    const objectKeys = Object.keys(object);
-    const filteredKeys = objectKeys.filter(key => keys.includes(key));
-    return filteredKeys.reduce((previous, current) => ({...previous, [current]: object[current]}), {});
-};
-
-module.exports = {filterKeys};
