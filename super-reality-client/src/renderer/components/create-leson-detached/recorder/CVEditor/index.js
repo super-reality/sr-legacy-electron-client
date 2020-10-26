035b5ca05ae579ab1505c9bb0e0893191a6f8f7a
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-10-26 11:24:59 -0300

    lint cveditor

--- super-reality-client/src/renderer/components/create-leson-detached/recorder/CVEditor/index.js
+++ super-reality-client/src/renderer/components/create-leson-detached/recorder/CVEditor/index.js
@@ -1,25 +1,21 @@
 export default class CVEditor {
   constructor(video, canvas) {
-  this._vid = null;
-  this._canvas = null;
-  this._context = null;
-  this.canvasElement = canvas;
-  this.videoElement = video;
+    this._vid = null;
+    this._canvas = null;
+    this._context = null;
+    this.canvasElement = canvas;
+    this.videoElement = video;
   }
-
 
   set videoElement(elem) {
-    if (elem === undefined)
-      throw new Error("video element undefined");
+    if (elem === undefined) throw new Error("video element undefined");
 
     this._vid = elem;
-    this._vid.addEventListener('seeked', this.videoEventHandler.bind(this));
+    this._vid.addEventListener("seeked", this.videoEventHandler.bind(this));
   }
 
-
   set canvasElement(elem) {
-    if (elem === undefined)
-      throw new Error("canvas element undefined");
+    if (elem === undefined) throw new Error("canvas element undefined");
     this._canvas = elem;
   }
 
@@ -28,12 +24,17 @@ export default class CVEditor {
       throw new Error("Interval is not a number");
     this._vid.currentTime = interval;
   }
-  
 
-  videoEventHandler(){
-    this._context = this._canvas.getContext('2d');
+  videoEventHandler() {
+    this._context = this._canvas.getContext("2d");
     this._canvas.width = this._vid.videoWidth;
     this._canvas.height = this._vid.videoHeight;
-    this._context.drawImage(this._vid, 0, 0, this._canvas.width, this._canvas.height);
+    this._context.drawImage(
+      this._vid,
+      0,
+      0,
+      this._canvas.width,
+      this._canvas.height
+    );
   }
 }
