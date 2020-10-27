de611019b9f768a149e8e8d0632374736baad4d1
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-10-27 16:06:31 -0300

    Update getFace.ts

--- super-reality-client/src/utils/getFace.ts
+++ super-reality-client/src/utils/getFace.ts
@@ -15,25 +15,26 @@ export default function getFace(
   const filename = `${userData}/output.mp4`;
 
   const data = new FormData();
-  if (image.files) data.append("image", image.files[0]);
-  if (video.files) data.append("image", video.files[0]);
-  /*
-  const imageFileContent = fs.readFileSync(image);
-  const imageFile = new File([imageFileContent], image);
-  data.append("image", imageFile);
+  // data.append('image', fs.createReadStream('/C:/Users/Desktop/face.jpg'));
+  // data.append('video', fs.createReadStream('/C:/Users/Desktop/video.mp4'));
 
-  const videoFileContent = fs.readFileSync(image);
-  const videoFile = new File([videoFileContent], image);
-  data.append("video", videoFile);
-  */
+  if (image.files) data.append("image", image.files[0]);
+  if (video.files) data.append("video", video.files[0]);
 
   setLoading(true);
 
-  Axios.post<string>(`http://3.101.43.24:5000/face_api`, data)
-    .then(handleGetFace)
-    .then((d) => {
-      console.log(d);
+  Axios.post<string>(`http://3.101.43.24:5000/face_api`, data, {
+    timeout: 1000 * 60,
+  })
+    .then((response) => {
+      console.log(response);
       setLoading(false);
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "file.mp4");
+      document.body.appendChild(link);
+      link.click();
       /*
       downloadFile(url, filename)
         .then(console.log)
