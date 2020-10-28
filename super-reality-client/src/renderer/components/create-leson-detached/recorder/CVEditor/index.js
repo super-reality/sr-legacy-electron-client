6b200f394b395a301f4d1b90d3a17a549adc5ad9
Taimoorkhokhar94 <taimoorkhokhar1994@gmail.com>
2020-10-28 19:51:50 +0500

    add trimAudio function in CVEditor class

--- super-reality-client/src/renderer/components/create-leson-detached/recorder/CVEditor/index.js
+++ super-reality-client/src/renderer/components/create-leson-detached/recorder/CVEditor/index.js
@@ -1,3 +1,23 @@
+
+/*
+We are using ffmpeg to trim audio. 
+You need to install these pakages: 
+-- npm install ffmpeg-static
+-- npm install any-shell-escape
+
+trimAudio function
+@params
+trimFrom = "00:00:00" or "seconds"
+trimTo   = "00:00:00" or "seconds"
+src      = "../folder/fileToTrim.webm"
+dst      = "../folder/trimmedAudio.webm" 
+
+*/
+
+const pathToFfmpeg = require('ffmpeg-static');
+const shell        = require('any-shell-escape')
+const {exec}       = require('child_process')
+
 export default class CVEditor {
   constructor(video, canvas) {
     this._vid = null;
@@ -36,5 +56,22 @@ export default class CVEditor {
       this._canvas.width,
       this._canvas.height
     );
+  }
+
+  trimAudio(trimFrom, trimTo, src, dst){
+    const ffmpegCommand = shell([
+      pathToFfmpeg,
+      '-ss', trimFrom, '-i', src,
+      '-t', trimTo, '-c', 'copy', dst
+    ])
+    
+    exec(ffmpegCommand, (err) => {
+      if (err) {
+        console.error(err)
+        process.exit(1)
+      } else {
+        console.info('Audio Trimmed!')
+      }
+    })
   }
 }
