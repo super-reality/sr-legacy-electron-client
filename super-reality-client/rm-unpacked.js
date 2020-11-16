b09574da31aa884724746a833271df5d92f2352d
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-11-16 20:17:02 -0300

    Update rm-unpacked.js

--- super-reality-client/rm-unpacked.js
+++ super-reality-client/rm-unpacked.js
@@ -1,27 +1,18 @@
 const fs = require("fs");
-const path = require("path");
 
 fs.rmdirSync("dist/win-unpacked", { recursive: true });
 
-const { exec } = require("child_process");
+const branchName =
+  process.env.TRAVIS_BRANCH || process.env.TRAVIS_BUILD_NUMBER || "";
+console.log(`Current branch is "${branchName}"`);
 
-exec("git branch --show-current", (err, stdout, stderr) => {
-  if (err) {
-    console.error(err);
-  }
-  if (typeof stdout === "string") {
-    const branchName = stdout.trim();
-    console.log(`Current branch is "${branchName}"`);
+const files = fs.readdirSync("dist/");
 
-    const files = fs.readdirSync("dist/");
-
-    files.forEach((file) => {
-      const splitFile = file.split(".");
-      const fileFormat = splitFile[splitFile.length - 1];
-      const newName = `${splitFile
-        .slice(0, -1)
-        .join(".")}-${branchName}.${fileFormat}`;
-      fs.rename(`dist/${file}`, `dist/${newName}`, (e) => console.error(e));
-    });
-  }
+files.forEach((file) => {
+  const splitFile = file.split(".");
+  const fileFormat = splitFile[splitFile.length - 1];
+  const newName = `${splitFile
+    .slice(0, -1)
+    .join(".")}-${branchName}.${fileFormat}`;
+  fs.rename(`dist/${file}`, `dist/${newName}`, (e) => console.error(e));
 });
