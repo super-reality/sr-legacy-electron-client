4891e3419eb80dc32c6737288cc3207529a4c44f
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-11-24 13:41:48 -0300

    update opencv test

--- super-reality-client/src/types/ipc.ts
+++ super-reality-client/src/types/ipc.ts
@@ -48,4 +48,3 @@ export type IpcArgument =
   | IpcMsgPythocResponse
   | ipcMsgCv
   | ipcMsgCvResult;
-
--- super-reality-client/src/utils/__tests__/cv.ts
+++ super-reality-client/src/utils/__tests__/cv.ts
@@ -1,14 +1,14 @@
 /* eslint-env jest */
-/*
-jest.mock("../opencv/opencv", () => {
-  // eslint-disable-next-line global-require
-  return require("opencv4nodejs-prebuilt");
-});
-*/
-
 import path from "path";
 import { AppState } from "../../renderer/redux/stores/renderer";
 import doCvMatch from "../cv/doCVMatch";
+
+jest.mock("../opencv/opencv", () => {
+  // eslint-disable-next-line global-require
+  // return require("opencv4nodejs-prebuilt");
+  // eslint-disable-next-line no-undef
+  return __non_webpack_require__("opencv4nodejs-prebuilt");
+});
 
 function isBetween(x: number, min: number, max: number) {
   return x >= min && x <= max;
