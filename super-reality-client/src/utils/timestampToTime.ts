9f73f5902a570f62585959ee80dd023d2dcea52f
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2020-11-30 16:07:21 -0300

    fix timestampToTime

--- super-reality-client/src/utils/timestampToTime.ts
+++ super-reality-client/src/utils/timestampToTime.ts
@@ -1,5 +1,13 @@
 /* eslint-disable radix */
 
 export default function timestampToTime(timestamp: string): number {
-  return parseInt(timestamp.replace(/:/g, ""));
+  // return parseInt(timestamp.replace(/:/g, ""));
+
+  const timestampFormat = timestamp.split(":");
+  const seconds =
+    +timestampFormat[0] * 60 * 60 +
+    +timestampFormat[1] * 60 +
+    +timestampFormat[2];
+  const milliSeconds = timestampFormat[3];
+  return seconds * 1000 + parseInt(milliSeconds);
 }
