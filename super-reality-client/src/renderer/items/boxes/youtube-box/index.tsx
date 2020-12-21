7c05d5524244515049d6e3fa79e5683d5fa2d346
Manwe <manuel.etchegaray7@gmail.com>
2020-12-21 10:50:09 -0300

    click though on editor

--- super-reality-client/src/renderer/items/boxes/youtube-box/index.tsx
+++ super-reality-client/src/renderer/items/boxes/youtube-box/index.tsx
@@ -1,14 +1,19 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import { useSelector } from "react-redux";
 import { ItemImageTriggers, ItemVideo } from "../../item";
 import { voidFunction } from "../../../constants";
 import ButtonSimple from "../../../components/button-simple";
 import "./index.scss";
 import { BaseBoxProps } from "../boxes";
+import { AppState } from "../../../redux/stores/renderer";
 
 const YoutubeBox = React.forwardRef<HTMLDivElement, BaseBoxProps<ItemVideo>>(
   (props, forwardedRef) => {
     const { item, style, pos, callback } = props;
+    const { previewing } = useSelector(
+      (state: AppState) => state.createLessonV2
+    );
 
     return (
       <div
@@ -25,6 +30,7 @@ const YoutubeBox = React.forwardRef<HTMLDivElement, BaseBoxProps<ItemVideo>>(
         <iframe
           src={`https://www.youtube.com/embed/${item.url}`}
           style={{
+            pointerEvents: previewing ? "all" : "none",
             maxHeight: item.trigger ? "calc(100% - 64px)" : "100%",
           }}
           frameBorder="0"
