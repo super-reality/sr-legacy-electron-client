63b721e3003cae54210de36e590fb0aa47f4df3a
Manwe <manuel.etchegaray7@gmail.com>
2020-12-22 12:09:37 -0300

    eslint

--- super-reality-client/src/utils/capture.ts
+++ super-reality-client/src/utils/capture.ts
@@ -7,25 +7,27 @@ export function capturePrimaryStream(): Promise<MediaStream> {
   const primaryId = `${getPrimaryMonitor().id}`;
 
   return new Promise((resolve, reject) => {
-    desktopCapturer.getSources({ types: ["screen"] }).then(async (sources: any) => {
-      const sourceId = sources.filter(
-        (source: any) => source.display_id == primaryId
-      )[0].id;
-      try {
-        const videoStream = await navigator.mediaDevices.getUserMedia({
-          audio: false,
-          video: {
-            mandatory: {
-              chromeMediaSource: "desktop",
-              chromeMediaSourceId: sourceId,
-            },
-          } as any,
-        });
-        resolve(videoStream);
-      } catch (e) {
-        reject(e);
-      }
-    });
+    desktopCapturer
+      .getSources({ types: ["screen"] })
+      .then(async (sources: any) => {
+        const sourceId = sources.filter(
+          (source: any) => source.display_id == primaryId
+        )[0].id;
+        try {
+          const videoStream = await navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: {
+              mandatory: {
+                chromeMediaSource: "desktop",
+                chromeMediaSourceId: sourceId,
+              },
+            } as any,
+          });
+          resolve(videoStream);
+        } catch (e) {
+          reject(e);
+        }
+      });
   });
 }
 
