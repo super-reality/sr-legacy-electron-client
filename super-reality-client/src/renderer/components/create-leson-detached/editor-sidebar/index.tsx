024ac13a027c13e74d2f2b904c0e9d08c7c92a6e
Manwe <manuel.etchegaray7@gmail.com>
2020-12-24 10:21:32 -0300

    dummy sidebar components

--- super-reality-client/src/renderer/components/create-leson-detached/editor-sidebar/index.tsx
+++ super-reality-client/src/renderer/components/create-leson-detached/editor-sidebar/index.tsx
@@ -2,15 +2,58 @@ import React, { useState } from "react";
 import "./index.scss";
 import { animated, useSpring } from "react-spring";
 
-export default function EditorSidebar() {
-  const [expaded, setExpanded] = useState(false);
+import { ReactComponent as DummyOne } from "../../../../assets/svg/new-fx-icon.svg";
+import { ReactComponent as DummyTwo } from "../../../../assets/svg/add-video.svg";
+import ButtonRound from "../../button-round";
 
-  const props = useSpring({ width: expaded ? "240px" : "0px" });
+const sidebarIcons = [
+  {
+    title: "dummy one",
+    icon: DummyOne,
+    component: <>Dummy One</>,
+  },
+  {
+    title: "dummy two",
+    icon: DummyTwo,
+    component: (
+      <>{`Dummy Two Title! put a whole jsx component here, like "<Component>"`}</>
+    ),
+  },
+];
+
+export default function EditorSidebar() {
+  const [expanded, setExpanded] = useState(false);
+  const [current, setCurrent] = useState(0);
+
+  const props = useSpring({
+    width: expanded ? "300px" : "0px",
+    minWidth: expanded ? "300px" : "0px",
+  });
 
   return (
     <>
-      <animated.div style={props} className="sidebar-expanded" />
-      <div className="sidebar-buttons" onClick={() => setExpanded(!expaded)} />
+      <animated.div style={props} className="sidebar-expanded">
+        <div className="sidebar-content">
+          {sidebarIcons[current]?.component}
+        </div>
+      </animated.div>
+      <div className="sidebar-buttons">
+        {sidebarIcons.map((icon, index) => {
+          return (
+            <ButtonRound
+              onClick={() => {
+                setCurrent(index);
+                if (index == current || !expanded) setExpanded(!expanded);
+              }}
+              width="32px"
+              height="32px"
+              key={icon.title}
+              svg={icon.icon}
+              title={icon.title}
+            />
+          );
+        })}
+      </div>
     </>
   );
 }
