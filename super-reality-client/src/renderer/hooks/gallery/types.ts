426469bdf979bd1263eda6389b0866e770a918f3
Manwe <manuel.etchegaray7@gmail.com>
2020-12-24 10:47:00 -0300

    useGallery mockup

---
+++ super-reality-client/src/renderer/hooks/gallery/types.ts
@@ -0,0 +1,5 @@
+export interface GalleryItem {
+  title: string;
+  thumbnail: string;
+  tags: string[];
+}
---
+++ super-reality-client/src/renderer/hooks/gallery/useGalleryPopup.tsx
@@ -0,0 +1,39 @@
+import React, { useCallback, useState } from "react";
+import "./gallery.scss";
+import usePopup from "../usePopup";
+import { GalleryItem } from "./types";
+import { InputChangeEv } from "../../../types/utils";
+
+interface GalleryProps {
+  title?: string;
+}
+
+export default function useGalleryPopup(
+  items: GalleryItem[],
+  callback: (selected: GalleryItem | null) => void
+): [(props: GalleryProps) => JSX.Element, number | null, () => void] {
+  const [GalleryPopup, doOpen, close] = usePopup(false);
+  const [inputValue, setInputValue] = useState("");
+  const [selected, _setSelected] = useState<null | number>(null);
+
+  const _clickOk = useCallback(() => {
+    close();
+    callback(selected ? items[selected] : null);
+  }, [close]);
+
+  const onChange = useCallback(
+    (e: InputChangeEv) => setInputValue(e.currentTarget.value),
+    []
+  );
+
+  const Gallery = () => {
+    return (
+      <GalleryPopup width="300px" height="200px">
+        <input value={inputValue} onChange={onChange} onKeyDown={onChange} />
+        {}
+      </GalleryPopup>
+    );
+  };
+
+  return [Gallery, selected, doOpen];
+}
