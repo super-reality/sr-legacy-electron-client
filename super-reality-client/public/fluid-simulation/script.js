cf7ebe82a410ed9fe70e5b9853c77817da6aa014
Manwe <manuel.etchegaray7@gmail.com>
2020-12-29 13:04:15 -0300

    hide fluids GUI and extras

--- super-reality-client/public/fluid-simulation/script.js
+++ super-reality-client/public/fluid-simulation/script.js
@@ -59,8 +59,8 @@ resizeCanvas();
 
 const config = {
   SIM_RESOLUTION: 128,
-  DYE_RESOLUTION: 512,
-  CAPTURE_RESOLUTION: 512,
+  DYE_RESOLUTION: 128,
+  CAPTURE_RESOLUTION: 128,
   DENSITY_DISSIPATION: 0.6,
   VELOCITY_DISSIPATION: 0.2,
   PRESSURE: 0.8,
@@ -114,7 +114,7 @@ if (!ext.supportLinearFiltering) {
   config.SUNRAYS = false;
 }
 
-startGUI();
+// startGUI();
 
 function getWebGLContext(canvas) {
   const params = {
@@ -274,80 +274,7 @@ function startGUI() {
     .onFinishChange(updateKeywords);
   sunraysFolder.add(config, "SUNRAYS_WEIGHT", 0.3, 1.0).name("weight");
 
-  const captureFolder = gui.addFolder("Capture");
-  captureFolder.addColor(config, "BACK_COLOR").name("background color");
-  captureFolder.add(config, "TRANSPARENT").name("transparent");
-  captureFolder.add({ fun: captureScreenshot }, "fun").name("take screenshot");
-
-  const github = gui
-    .add(
-      {
-        fun: () => {
-          window.open("https://github.com/PavelDoGreat/WebGL-Fluid-Simulation");
-          ga("send", "event", "link button", "github");
-        },
-      },
-      "fun"
-    )
-    .name("Github");
-  github.__li.className = "cr function bigFont";
-  github.__li.style.borderLeft = "3px solid #8C8C8C";
-  const githubIcon = document.createElement("span");
-  github.domElement.parentElement.appendChild(githubIcon);
-  githubIcon.className = "icon github";
-
-  const twitter = gui
-    .add(
-      {
-        fun: () => {
-          ga("send", "event", "link button", "twitter");
-          window.open("https://twitter.com/PavelDoGreat");
-        },
-      },
-      "fun"
-    )
-    .name("Twitter");
-  twitter.__li.className = "cr function bigFont";
-  twitter.__li.style.borderLeft = "3px solid #8C8C8C";
-  const twitterIcon = document.createElement("span");
-  twitter.domElement.parentElement.appendChild(twitterIcon);
-  twitterIcon.className = "icon twitter";
-
-  const discord = gui
-    .add(
-      {
-        fun: () => {
-          ga("send", "event", "link button", "discord");
-          window.open("https://discordapp.com/invite/CeqZDDE");
-        },
-      },
-      "fun"
-    )
-    .name("Discord");
-  discord.__li.className = "cr function bigFont";
-  discord.__li.style.borderLeft = "3px solid #8C8C8C";
-  const discordIcon = document.createElement("span");
-  discord.domElement.parentElement.appendChild(discordIcon);
-  discordIcon.className = "icon discord";
-
-  const app = gui
-    .add(
-      {
-        fun: () => {
-          ga("send", "event", "link button", "app");
-          window.open("http://onelink.to/5b58bn");
-        },
-      },
-      "fun"
-    )
-    .name("Check out mobile app");
-  app.__li.className = "cr function appBigFont";
-  app.__li.style.borderLeft = "3px solid #00FF7F";
-  const appIcon = document.createElement("span");
-  app.domElement.parentElement.appendChild(appIcon);
-  appIcon.className = "icon app";
-
-  if (isMobile()) gui.close();
+  gui.close();
 }
 
 function isMobile() {
