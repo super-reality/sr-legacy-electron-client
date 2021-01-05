f9de3fa8b4323f847ab6e2679236a37248707669
Manwe <manuel.etchegaray7@gmail.com>
2021-01-05 11:03:09 -0300

    Update index.tsx

--- super-reality-client/src/renderer/views/test/index.tsx
+++ super-reality-client/src/renderer/views/test/index.tsx
@@ -2,7 +2,6 @@ import React, { useCallback } from "react";
 import "./index.scss";
 import { useDispatch } from "react-redux";
 import ButtonSimple from "../../components/button-simple";
-import ipcSend from "../../../utils/ipcSend";
 import { MODE_LESSON_CREATOR } from "../../redux/slices/renderSlice";
 import setAppMode from "../../redux/utils/setAppMode";
 
@@ -13,6 +12,7 @@ export default function Test(): JSX.Element {
     setAppMode(MODE_LESSON_CREATOR);
   }, [dispatch]);
 
+  /*
   const pythonTest = useCallback(() => {
     ipcSend({
       method: "pythonExec",
@@ -20,20 +20,12 @@ export default function Test(): JSX.Element {
       to: "background",
     });
   }, []);
+  */
 
   return (
     <div className="mid">
       <ButtonSimple width="200px" height="24px" margin="auto" onClick={onClick}>
-        Click me!
-      </ButtonSimple>
-
-      <ButtonSimple
-        width="200px"
-        height="24px"
-        margin="auto"
-        onClick={pythonTest}
-      >
-        Test python background
+        Create Lesson
       </ButtonSimple>
     </div>
   );
