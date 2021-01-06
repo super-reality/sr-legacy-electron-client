131318845d9f094c60404440bff26696ec6ec86a
Manwe <manuel.etchegaray7@gmail.com>
2021-01-06 10:22:06 -0300

    Update aws.ts

--- super-reality-client/src/utils/__tests__/aws.ts
+++ super-reality-client/src/utils/__tests__/aws.ts
@@ -5,15 +5,18 @@ require("dotenv").config();
 
 test("Can access Amazon S3 builds", async (done) => {
   const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_KEY,
-    secretAccessKey: process.env.AWS_SECRET,
+    accessKeyId: process.env.ARTIFACTS_KEY,
+    secretAccessKey: process.env.ARTIFACTS_SECRET,
   });
 
   await new Promise((resolve, reject) =>
-    s3.listObjects({ Bucket: "super-reality-builds" }, (err: any, d) => {
-      if (err) reject();
-      else resolve(d);
-    })
+    s3.listObjects(
+      { Bucket: process.env.ARTIFACTS_BUCKET || "super-reality-builds" },
+      (err: any, d) => {
+        if (err) reject();
+        else resolve(d);
+      }
+    )
   )
     .then(() => done())
     .catch(console.error);
