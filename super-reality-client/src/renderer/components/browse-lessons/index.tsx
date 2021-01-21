d6dbee58b8c60ffeb9d4397061936aa87054edd5
Manwe <manuel.etchegaray7@gmail.com>
2021-01-21 12:42:27 -0300

    BrowseLessons page

---
+++ super-reality-client/src/renderer/components/browse-lessons/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./index.scss";
+
+import closeWindow from "../../../utils/electron/closeWindow";
+import minimizeWindow from "../../../utils/electron/minimizeWindow";
+import Windowlet from "../windowlet";
+import LessonPreview from "./lesson-preview";
+import ReactSelect from "../top-select";
+import { voidFunction } from "../../constants";
+
+const options = ["My Tutorials", "Shared with me", "Public"];
+
+export default function BrowseLessons() {
+  return (
+    <Windowlet
+      width={1100}
+      height={600}
+      title="Super Reality"
+      onMinimize={minimizeWindow}
+      onClose={closeWindow}
+    >
+      <div className="browse-lessons-window">
+        <div className="controls">
+          <div>Tutorials</div>
+          <ReactSelect
+            style={{ width: "160px", marginLeft: "32px" }}
+            current={options[0]}
+            options={options}
+            callback={voidFunction}
+          />
+        </div>
+        <div className="grid">
+          <LessonPreview title="How to make Pong" />
+          <LessonPreview title="This is a dummy" />
+          <LessonPreview title="Another dummy" />
+          <LessonPreview title="Learn how to make CS:GO" />
+          <LessonPreview title="Make fun stuff" />
+        </div>
+      </div>
+    </Windowlet>
+  );
+}
