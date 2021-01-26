916871696ff0e7fe9a4898e45d22d3bfa543b897
Manwe <manuel.etchegaray7@gmail.com>
2021-01-26 15:57:17 -0300

    Update supportTicket.ts

--- super-reality-client/src/renderer/api/types/support-ticket/supportTicket.ts
+++ super-reality-client/src/renderer/api/types/support-ticket/supportTicket.ts
@@ -1,14 +1,15 @@
-import { CodeSuccess, ApiSucess } from "..";
+/* eslint-disable camelcase */
+import { CodeSuccess } from "..";
 
 export default interface supportTicker {
   supportType?: TsupportType;
-  title?:string;
-  category?:string;
+  title?: string;
+  category?: string;
   skills?: string[];
   description?: string;
   images?: File[];
-  newSkills?:string[];
-  newCategory?:string;
+  newSkills?: string[];
+  newCategory?: string;
   skillsData?: IData[];
   searchedSkills?: IData[];
   categoryData?: IData[];
@@ -17,52 +18,53 @@ export default interface supportTicker {
 export interface IData {
   name: string;
   id: string;
-  new?:boolean;
+  new?: boolean;
 }
 
-export interface IDataGet{
+export interface IDataGet {
   name: string;
   _id: string;
 }
 
-export interface ISkillsGet{
+export interface ISkillsGet {
   err_code: CodeSuccess;
   skill: IDataGet[];
 }
 
-export interface ICategoryGet{
+export interface ICategoryGet {
   err_code: CodeSuccess;
   category: IData[];
 }
 
 export interface Ioptions {
-  id:string;
-  name:string;
+  id: string;
+  name: string;
 }
-
-
 
 export interface Icategory {
-  name:string;
-  id:string;
+  name: string;
+  id: string;
 }
-
 
 export interface tags {
   name: string;
 }
-export type TsupportType = "help_short" | "help_long" | "build" | "question" | "" | undefined
+export type TsupportType =
+  | "help_short"
+  | "help_long"
+  | "build"
+  | "question"
+  | ""
+  | undefined;
 interface option {
   name: string;
   description: string;
 }
 interface category {
-  name: String;
+  name: string;
 }
 
 interface requestCategory {
   name: string;
   tags: tags[];
 }
-
-
