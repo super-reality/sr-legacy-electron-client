75bd809f2d0af2c827b57c5de88b86be9aad8a14
Manwe <manuel.etchegaray7@gmail.com>
2021-01-27 12:25:38 -0300

    open sidebar subcomponent

--- super-reality-client/src/renderer/components/sidebar/index.tsx
+++ super-reality-client/src/renderer/components/sidebar/index.tsx
@@ -131,6 +131,10 @@ export default function Sidebar() {
       if (icon.subComponent) {
         setWideView(current == id ? !wideView : true);
       }
+      if (!icon.subComponent && icon.component && !icon.onClick) {
+        setCurrent(id);
+        setContentExpanded(!contentExpanded);
+      }
       if (icon.onClick) {
         icon.onClick();
       }
