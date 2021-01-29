9b467b96e3a4b5eb09666d457f6a71365c46d903
Manwe <manuel.etchegaray7@gmail.com>
2021-01-29 14:36:55 -0300

    style(eslint): sidebar and help missing prettier

--- src/renderer/components/sidebar/index.tsx
+++ src/renderer/components/sidebar/index.tsx
@@ -21,7 +21,7 @@ import useLessonPlayer from "../lesson-player/useLessonPlayer";
 import { AppState } from "../../redux/stores/renderer";
 import GroupsList from "./groups-list";
 import ActionButtons from "./action-buttons";
-import Support from '../support';
+import Support from "../support";
 
 export interface SidebarIcon {
   title: string;
@@ -93,7 +93,7 @@ export default function Sidebar() {
       {
         title: "Support",
         icon: TutorialsIcon,
-        component: <Support/>,
+        component: <Support />,
         subComponent: null,
         componentWidth: 900,
       },
