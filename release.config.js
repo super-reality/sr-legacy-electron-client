0ae73c5f170aca8bf345db41777f29d790a9c356
Manwe <manuel.etchegaray7@gmail.com>
2021-03-17 19:35:40 -0300

    fix(ci): release config

---
+++ release.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-template-curly-in-string */
+module.exports = {
+  plugins: [
+    "@semantic-release/github",
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "docs/CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["docs/CHANGELOG.md", "package.json", "package-lock.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
