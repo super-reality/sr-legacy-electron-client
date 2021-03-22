d4e8f130fd7e1afd56f496d70345af59171974bf
denisGreen <kolomiitsev.denis@gmail.com>
2021-03-22 20:56:16 +0200

    integration of xrengine * add xrengine client * install dependencies

--- src/renderer/components/support/support-menu/index.tsx
+++ src/renderer/components/support/support-menu/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.scss";
+
 import Vivus from "vivus";
 import { ReactComponent as SupportIcon } from "../../../../assets/svg/support-icon.svg";
 import SupportAskIcon from "../../../../assets/svg/support-ask.svg";
---
+++ src/renderer/xrengine/xrconstants.ts
@@ -0,0 +1,15 @@
+export const xrApiServer = "https://api-dev.theoverlay.io/openapi/";
+export const featherStoreKey = "xrfeatherStore";
+export const localStorageKey = "theoverlay-client-store";
+
+export const auth = {
+  enableSmsMagicLink: true,
+  enableEmailMagicLink: true,
+  enableUserPassword: true,
+  enableGithubSocial: true,
+  enableFacebookSocial: true,
+  enableGoogleSocial: true,
+};
+export const alert = {
+  timeout: 10000,
+};
